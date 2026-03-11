f92fb9d92814aaca3e6b1ef4655e555d41d24419
FromSunNews <phuongtu590@gmail.com>
2026-03-11 12:23:38 +0700

    adjus pm2

--- src/infra/redis/redis.module.ts
+++ src/infra/redis/redis.module.ts
@@ -11,14 +11,14 @@ import { RedisService } from './redis.service';
       useFactory: (...args: unknown[]): RedisModuleOptions => {
         const [configService] = args as [ConfigService];
         const config: any = {
-          host: configService.get<string>('REDIS_HOST'),
-          port: configService.get<number>('REDIS_PORT'),
-          password: configService.get<string>('REDIS_PASSWORD') || undefined,
-          db: configService.get<number>('REDIS_DB') || 0,
+          host: configService.get<string>('redis.host'),
+          port: configService.get<number>('redis.port'),
+          password: configService.get<string>('redis.password') || undefined,
+          db: configService.get<number>('redis.db') || 0,
           retryStrategy: (times: number) => Math.min(times * 50, 2000),
         };
 
-        if (configService.get<string>('REDIS_TLS') === 'true') {
+        if (configService.get<string>('redis.tls')) {
           config.tls = {};
         }
 
